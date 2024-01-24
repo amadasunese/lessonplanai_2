595bf9806de09380c6698b9e513e02d37f250ef9
Ese Amadasun <amadasunese@gmail.com>
2024-01-24 13:28:19 +0100

    config file updated

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = config("SECRET_KEY", default="guess-me")
+    SECRET_KEY = config("SECRET_KEY", default="can-you-guess-me?")
     SQLALCHEMY_DATABASE_URI = DATABASE_URI
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BCRYPT_LOG_ROUNDS = 13
     WTF_CSRF_ENABLED = True
     DEBUG_TB_ENABLED = False
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    SECURITY_PASSWORD_SALT = config("SECURITY_PASSWORD_SALT", default="very-important")
+    SECURITY_PASSWORD_SALT = config("SECURITY_PASSWORD_SALT", default="important")
 
     # Mail Settings
     MAIL_DEFAULT_SENDER = "noreply@flask.com"
